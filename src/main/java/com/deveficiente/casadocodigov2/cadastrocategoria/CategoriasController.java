c5409d7fb55520e4a7b503654190e0408ec7b41e
MBnhs <marcelo.scavassa@liferay.com>
2025-03-27 16:14:54 -0300

    Refatoring CategoriaController to use dependency injection via constructor.

--- src/main/java/com/deveficiente/casadocodigov2/cadastrocategoria/CategoriasController.java
+++ src/main/java/com/deveficiente/casadocodigov2/cadastrocategoria/CategoriasController.java
@@ -12,8 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CategoriasController {
 
+	/*
+	Before:
 	@PersistenceContext
 	private EntityManager manager;
+
+	 */
+
+	/*
+	Using dependency injection to make Categorias Controller more testable.
+	This way it's possible to use the Constructor to inject EntityManager and
+	mock it in a automated test.
+
+	After:
+	begin
+	 */
+	private EntityManager manager;
+
+	public CategoriasController(EntityManager manager) {
+		this.manager = manager;
+	}
+	/*
+	end
+	 */
 
 	@PostMapping(value = "/categorias")
 	@Transactional
